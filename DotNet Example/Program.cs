c90231f86b7581b9ab3a6c7d7f486e03f0496531
berke <berke6161@icloud.com>
2023-03-05 04:10:30 +0300

    update

--- DotNet Example/Program.cs
+++ DotNet Example/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int s1, s2, toplam;
 
+
+Console.Title = "Berke Çırakman";
+
+Console.WriteLine("1. Sayıyı girin");
+s1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("2. Sayıyı girin");
+s2 = Convert.ToInt32(Console.ReadLine());
+
+toplam = s1 + s2;
+
+Console.WriteLine("iki sayının toplamı" + toplam + "dir");
+
+Console.ReadKey();
